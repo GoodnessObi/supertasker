60234a04070f8ae27bbe9864bd8526ec842d5ce2
GoodnessObi <goodnessobi@gmail.com>
2023-09-26 14:35:16 +0100

    type useselector hook and create custom tyoed useselector and usedispatch hooks

--- src/store.ts
+++ src/store.ts
@@ -2,7 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { taskReducer } from './features/tasks-slice';
 
 const store = configureStore({
-  reducer: taskReducer,
+  reducer: {
+    tasks: taskReducer,
+  },
 });
+
+export type ApplicationState = ReturnType<typeof store.getState>;
+
+export type ApplicationDispatch = typeof store.dispatch;
 
 export default store;
